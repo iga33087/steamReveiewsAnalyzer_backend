d8904ae37b1cb0760d8e9fb1f169347c8040d764
iga high <iga33087@gmail.com>
2026-05-04 22:53:33 +0800

    fix

--- lib/Review.py
+++ lib/Review.py
@@ -2,6 +2,10 @@ import requests
 from urllib.parse import quote
 from datetime import date
 
+steamApiBase = 'https://store.steampowered.com/appreviews/'
+openWebUIApiBase = 'http://localhost:3000'
+openWebUIApiKey = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjUwMDgxN2U0LWU4YmItNGY4MC04OTk3LTQyYTgxNzk5NTFkMyIsImV4cCI6MTc3ODY3ODc2NSwianRpIjoiZjFkODU1YWEtNDA1ZS00NGI5LWEwZDUtZmVlZDFlYjZjODdmIn0.KxPyh3_P2nBHyd4zGKLfuCaSdnd2w2LHnwfCvOATtTE'
+
 class Review:
     def __init__(self, id):
         self.id = id
@@ -23,7 +27,7 @@ class Review:
             cursor.append(res['cursor'])
             obj['cursor'] = res['cursor']
             self.data.extend(res['reviews'])
-            res = requests.get(f'https://store.steampowered.com/appreviews/{self.id}',obj).json()
+            res = requests.get(f'{steamApiBase}{self.id}',params=obj).json()
             if 'review_score' in res['query_summary']:
                 self.total = res['query_summary']
     
@@ -53,6 +57,29 @@ class Review:
                 res[dateTime]['all']['voted_down'] += 1
                 res[dateTime][x['language']]['voted_down'] += 1
         return res
+
+    def getReveiwsArr(self):
+        res = []
+        for x in self.data:
+            obj = {
+                'review': x['review'],
+                'language': x['language'],
+                'positives': x['voted_up'],
+            }
+            res.append(obj)
+        return res
+
+    def getLLMReport(self):
+        headers = {
+            'Authorization': openWebUIApiKey,
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'model': 'llama3:latest',
+            'messages': [{'role': 'user', 'content': f'{self.getReveiwsArr()} 這是某一款遊戲的steam評論，幫我從上面這些評論來統整出這款遊戲的整體評價，用中文回覆'}]
+        }
+        res = requests.post(f'{openWebUIApiBase}/api/chat/completions',headers=headers,json=data).json()
+        return res
     
     def getData(self):
-        return {'total':self.total,'countryObj':self.getCountryObj(),'timeObj':self.getTimeObj()}
+        return {'total':self.total,'countryObj':self.getCountryObj(),'timeObj':self.getTimeObj(),'data':self.getReveiwsArr()}
--- main.py
+++ main.py
@@ -8,8 +8,7 @@ def read_root():
     t1 = Review('2719200')
     return t1.getData()
 
-
 @app.get("/test")
 def read_item():
-    t1 = Review('590830')
-    return t1.getTimeObj()
+    t1 = Review('2719200')
+    return t1.getLLMReport()
